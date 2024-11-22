1eeb8628b1fd91e9a3b341bcb51d40012a862177
Daniel Kennedy <danielrkennedy03@gmail.com>
2024-11-22 00:01:10 +0000

    move wifi connection out

---
+++ lib/Wifi/Wifi.c
@@ -0,0 +1,60 @@
+#include "Wifi.h"
+
+// Wi-Fi event handler
+void wifi_event_handler(void *arg, esp_event_base_t event_base,
+                               int32_t event_id, void *event_data) {
+    wifi_event_handler_context_t *wifi_event_handler_context = (wifi_event_handler_context_t *)arg;
+    if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START) {
+        esp_wifi_connect();
+    } else if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_DISCONNECTED) {
+        if (wifi_event_handler_context->retry_count < MAX_RETRY) {
+            esp_wifi_connect();
+            wifi_event_handler_context->retry_count++;
+            ESP_LOGI(TAG, "Retrying connection to Wi-Fi...");
+        } else {
+            ESP_LOGE(TAG, "Failed to connect to Wi-Fi");
+        }
+    } else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP) {
+        ip_event_got_ip_t *event = (ip_event_got_ip_t *)event_data;
+        ESP_LOGI(TAG, "Got IP: " IPSTR, IP2STR(&event->ip_info.ip));
+        wifi_event_handler_context->retry_count = 0;
+        ESP_LOGI(TAG, "SETTING WIFI CONNECTED BIT");
+        xEventGroupSetBits(wifi_event_handler_context->wifi_event_group, wifi_event_handler_context->wifi_connected_bit);
+    }
+}
+
+// Initialize Wi-Fi
+void wifi_init_sta(wifi_event_handler_context_t *wifi_event_handler_context) {
+
+    ESP_ERROR_CHECK(esp_netif_init());
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+    esp_netif_create_default_wifi_sta();
+
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+
+    esp_event_handler_instance_t instance_any_id;
+    esp_event_handler_instance_t instance_got_ip;
+    ESP_ERROR_CHECK(esp_event_handler_instance_register(WIFI_EVENT,
+                                                        ESP_EVENT_ANY_ID,
+                                                        &wifi_event_handler,
+                                                        wifi_event_handler_context,
+                                                        &instance_any_id));
+    ESP_ERROR_CHECK(esp_event_handler_instance_register(IP_EVENT,
+                                                        IP_EVENT_STA_GOT_IP,
+                                                        &wifi_event_handler,
+                                                        wifi_event_handler_context,
+                                                        &instance_got_ip));
+
+    wifi_config_t wifi_config = {
+        .sta = {
+            .ssid = WIFI_SSID,
+            .password = WIFI_PASS,
+        },
+    };
+    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
+    ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_STA, &wifi_config));
+    ESP_ERROR_CHECK(esp_wifi_start());
+
+    ESP_LOGI(TAG, "Wi-Fi initialization complete.");
+}
--- lib/Wifi/Wifi.h
+++ lib/Wifi/Wifi.h
@@ -1,10 +1,19 @@
 #ifndef WIFI_H
 #define WIFI_H
 
+#include "defines.h"
+
+#include "esp_wifi.h"
+#include "esp_event.h"
+#include "esp_log.h"
+
 typedef struct {
     EventGroupHandle_t wifi_event_group;
     int wifi_connected_bit;
     int retry_count;
 } wifi_event_handler_context_t;
+
+void wifi_event_handler(void *arg, esp_event_base_t event_base, int32_t event_id, void *event_data);
+void wifi_init_sta(wifi_event_handler_context_t *wifi_event_handler_context);
 
 #endif // WIFI_H
