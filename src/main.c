f97e4d43faccfd9b871af224243425f8ffe3492e
Daniel Kennedy <danielrkennedy03@gmail.com>
2024-12-02 18:53:39 +0000

    sntp in main

--- src/main.c
+++ src/main.c
@@ -3,6 +3,7 @@
 // C standard libraries
 #include <stdio.h>
 #include <string.h>
+#include <time.h>
 
 // FreeRTOS
 #include "freertos/FreeRTOS.h"
@@ -13,6 +14,7 @@
 #include "esp_wifi.h"
 #include "esp_log.h"
 #include "nvs_flash.h"
+#include "esp_sntp.h"
 
 // Tasks
 #include "TCP_Server.h"
@@ -23,8 +25,43 @@
 
 #define TAG "main"
 
-#define WIFI false
+#define WIFI true
 #define SERVER false
+
+
+void time_sync_notification_cb(struct timeval *tv) {
+    ESP_LOGI(TAG, "Time synchronization event received");
+}
+
+void obtain_time(void) {
+    ESP_LOGI(TAG, "Initializing SNTP");
+
+    // Initialize the SNTP service
+    esp_sntp_setoperatingmode(SNTP_OPMODE_POLL);
+    esp_sntp_setservername(0, "pool.ntp.org"); // Use a default NTP server
+    esp_sntp_set_time_sync_notification_cb(time_sync_notification_cb);
+    esp_sntp_init();
+
+    // Wait for time to be set
+    time_t now = 0;
+    struct tm timeinfo = {0};
+    int retry = 0;
+    const int retry_count = 10;
+
+    while (timeinfo.tm_year < (2016 - 1900) && ++retry < retry_count) {
+        ESP_LOGI(TAG, "Waiting for system time to be set... (%d/%d)", retry, retry_count);
+        vTaskDelay(2000 / portTICK_PERIOD_MS); // Wait 2 seconds
+        time(&now);
+        localtime_r(&now, &timeinfo);
+    }
+
+    if (timeinfo.tm_year < (2016 - 1900)) {
+        ESP_LOGE(TAG, "Failed to synchronize time");
+    } else {
+        ESP_LOGI(TAG, "Time synchronized successfully");
+    }
+}
+
 
 void app_main(void) {
 
@@ -49,5 +86,19 @@ void app_main(void) {
         xTaskCreate(tcp_server_task, "tcp_server_task", 4096, NULL, 5, NULL);
     }
 
-    
+
+    obtain_time();
+
+    // Display synchronized time
+    time_t now;
+    struct tm timestamp;
+    char strftime_buf[64];
+
+    time(&now);
+    localtime_r(&now, &timestamp);
+
+    // Format and print the time
+    strftime(strftime_buf, sizeof(timestamp), "%Y-%m-%dT%H:%M:%S.000%Z", &timestamp);
+    ESP_LOGI(TAG, "The current date/time is: %s", strftime_buf);
+
 }
