6f8a7b14024576a04bdf248941a62fdd6f31a025
Daniel Kennedy <danielrkennedy03@gmail.com>
2024-12-14 12:16:39 +0000

    controller now waits on isr during command and on queue when not busy

--- src/Controller.c
+++ src/Controller.c
@@ -3,39 +3,33 @@
 #define TAG "Controller"
 
 static QueueHandles queue_handles;
-
 TaskHandle_t controller_task_handle;
-volatile bool timer_flag = false;
-
 esp_timer_handle_t timer;
 
+bool waiting_for_command = true;
+
 void IRAM_ATTR timerISR(void *arg) {
-    timer_flag = true;
+    vTaskNotifyGiveFromISR(controller_task_handle, NULL);
 }
 
 void init_timer(TaskHandle_t _controller_task_handle){
     ESP_LOGI(TAG, "Initializing timer");
     controller_task_handle = _controller_task_handle;
 
-    //ESP_ERROR_CHECK(esp_timer_init());
-
     esp_timer_create_args_t timer_args = {
         .callback = &timerISR,
         .arg = NULL,
-        .dispatch_method = ESP_TIMER_TASK,
+        .dispatch_method = ESP_TIMER_ISR,
         .name = "controller_timer"
     };
 
     ESP_ERROR_CHECK(esp_timer_create(&timer_args, &timer));
-
-    ESP_ERROR_CHECK(esp_timer_start_once(timer, 1 * SECOND_IN_US));
-
-    timer_flag = false;
 };
 
 
 void controller_task(void *pvParameters){
     
+    ESP_LOGI(TAG, "Controller task running");
     queue_handles = *(QueueHandles *)pvParameters;
 
     
@@ -47,51 +41,53 @@ void controller_task(void *pvParameters){
 
     while (1)
     {
-        ESP_LOGI(TAG, "Controller task running");
-        ESP_LOGI(TAG, "timer_flag: %d", timer_flag);
-
-        if (!timer_flag)
-        {
-            ESP_LOGW(TAG, "Timer not expired, take reading or whatever");
-            vTaskDelay(1000 / portTICK_PERIOD_MS);
-            continue;
+        if (!waiting_for_command){
+            // wait for the timer to expire
+            ESP_LOGI(TAG, "Waiting for timer to expire");
+            ulTaskNotifyTake(pdTRUE, portMAX_DELAY);
         }
 
         ESP_LOGI(TAG, "Waiting for command");
+        waiting_for_command = true;
 
         Command command;
 
-        if (xQueueReceive(queue_handles.command_queue, &command, 0) == pdTRUE)
+        // Default to shutting off the heater
+        HeaterState heater_state = get_heater_state();
+        heater_state.heater_on = false;
+        ThermostatState thermostat_state = get_thermostat_state();
+        thermostat_state.mode = THERMOSTAT_OFF;
+
+        ESP_LOGI(TAG, "Shutting off heater and thermostat");
+        set_heater_state(heater_state);
+        set_thermostat_state(thermostat_state);
+
+        // Wait for a command
+        if (xQueueReceive(queue_handles.command_queue, &command, portMAX_DELAY) == pdTRUE)
         {
             ESP_LOGI(TAG, "Received command");
+            waiting_for_command = false;
 
             switch (command.mode)
             {
             case SET_TARGET_TEMP_FOR_DURATION:
                 // Give thermostat control of the heater, and set the target temperature
                 ESP_LOGI(TAG, "Mode: SET_TARGET_TEMP_FOR_DURATION, Target Temp: %.2f, Duration: %d", command.data.target_temp, command.duration);
-                ThermostatState state = get_thermostat_state();
-                state.target_temp = command.data.target_temp;
-                state.mode = THERMOSTAT_ON;
-                set_thermostat_state(state);
+                thermostat_state.mode = THERMOSTAT_ON;
+                thermostat_state.target_temp = command.data.target_temp;
                 break;
             case SET_POWER_FOR_DURATION:
+                // Turn the heater on or off directly for a duration
                 ESP_LOGI(TAG, "Mode: SET_CONTROL_FOR_DURATION, Heater: %s, Duration: %d", command.data.heater_on ? "ON" : "OFF", command.duration);
-                set_heater_state((HeaterState){.heater_on = command.data.heater_on});
+                heater_state.heater_on = command.data.heater_on;
                 break;
             }
 
             ESP_LOGW(TAG, "Resetting timer, duration: %d", command.duration);
             esp_timer_stop(timer);
-            timer_flag = false;
             ESP_ERROR_CHECK(esp_timer_start_once(timer, command.duration * SECOND_IN_US));
-        } else {
-            ESP_LOGI(TAG, "No command received");
-            ESP_LOGI(TAG, "Shutting off heater");
-            set_heater_state((HeaterState){.heater_on = false});
-            set_thermostat_state((ThermostatState){.mode = THERMOSTAT_OFF});
         }
-
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
+        set_heater_state(heater_state);
+        set_thermostat_state(thermostat_state);
     }
 }
